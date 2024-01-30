fec0c599465405f0a2d6685ad56d661103f744ff
Piotr Grzegorzek <49654876+piotr-grzegorzek@users.noreply.github.com>
2024-01-30 23:06:43 +0100

    feat: materials

--- Assets/Scripts/RulesStrategy/Implementations/Base/AmericanRulesStrategy.cs
+++ Assets/Scripts/RulesStrategy/Implementations/Base/AmericanRulesStrategy.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 internal class AmericanRulesStrategy : RulesStrategy
 {
     internal override int BoardSize => 8;
-    internal override Color PlayableTileColor => Color.green;
+    internal override Color PlayableTileColor => new(0.47f, 0.68f, 0.51f);
     internal override int RowsPerTeam => 3;
-    internal override Color DarkPieceColor => Color.red;
+    internal override Color DarkPieceColor => new(0.73f, 0.25f, 0.25f);
     internal override GameColor StartingPieceColor => GameColor.Dark;
     internal override bool FlyingKing => false;
     internal override bool PawnCanCaptureBackwards => false;
--- Assets/Scripts/RulesStrategy/Implementations/Base/InternationalRulesStrategy.cs
+++ Assets/Scripts/RulesStrategy/Implementations/Base/InternationalRulesStrategy.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 internal class InternationalRulesStrategy : RulesStrategy
 {
     internal override int BoardSize => 10;
-    internal override Color PlayableTileColor => Color.black;
+    internal override Color PlayableTileColor => new(0.63f, 0.59f, 0.57f);
+
     internal override int RowsPerTeam => 4;
-    internal override Color DarkPieceColor => new Color(0.5f, 0.5f, 0.5f);
+    internal override Color DarkPieceColor => new(0.4f, 0.29f, 0.27f);
     internal override GameColor StartingPieceColor => GameColor.Light;
     internal override bool FlyingKing => true;
     internal override bool PawnCanCaptureBackwards => true;
